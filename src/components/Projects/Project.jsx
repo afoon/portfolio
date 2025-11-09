b90ceefa3cc5c90e3971b3fda93b1649bfb010ef
Amina <amefoon@gmail.com>
2025-11-09 05:49:11 -0500

    cover card to astro

--- src/components/Projects/Project.jsx
+++
@@ -1,9 +0,0 @@
-import ProjectCard from './ProjectCard'
-const Project = () => {
-    return (<div>
-        <h1 style={{color: "var(--color-primary)"}}>Projects</h1>
-        <ProjectCard/>
-    </div>)
-}
-
-export default Project
