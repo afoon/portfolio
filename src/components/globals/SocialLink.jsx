7ce4308ff683423c556f43828040b565e41286b6
Amina <amefoon@gmail.com>
2026-01-16 18:06:35 -0500

    increase link area

--- src/components/globals/SocialLink.jsx
+++ src/components/globals/SocialLink.jsx
@@ -1,4 +1,4 @@
-const SocialLink = ({ href, color="var(--color-bg-light)", bgColor="var(--color-blue-950)", icon, iconSize = "2rem", linkSize = "2rem" }) => {
+const SocialLink = ({ href, color="var(--color-bg-light)", bgColor="var(--color-blue-950)", icon, iconSize = "3rem", linkSize = "3rem" }) => {
   return (
     <a style={{fontSize: iconSize, border: "2px solid var(--color-blue-950)", borderRadius: "100%", padding: "0.5rem", boxShadow: "1.5px 2.5px 0 1px rgba(0, 0, 0, 0.25)", backgroundColor: color, width: linkSize, height: linkSize, display: "flex", justifyContent: "center", alignItems: "center"}} href={href} target="_blank" rel="noopener noreferrer">
       <i style={{ background: bgColor, backgroundClip: 'text', color: "transparent"}} className={`bxl bx-${icon}`}></i>
